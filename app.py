4bbda2785e5a0f0efedfe0bd075a4ff122a29364
ivetterojas-afk <ivette.rojas@casagrande.edu.ec>
2026-05-26 22:09:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ import seaborn as sns
 
 
 st.title("Proyecto final UCG")
-
 st.sidebar.title("Parámetros")
-
 st.sidebar.image("Python_logo.png")
+
+# Carga de datos
+st.sidebar.header("1. Carga del dataset")
+archivo = st.sidebar.file_uploader("Suba el archivo CSV", type=["csv"])
+
+if archivo:
+    df = pd.read_csv(archivo)
+
+    st.success("Dataset cargado correctamente")
+
+    # Previsualización
+    st.header("2. Previsualización del dataset")
+    st.dataframe(df.head())
