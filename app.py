58806ea8a017ef8c4ce68602baead23e18705680
ivetterojas-afk <ivette.rojas@casagrande.edu.ec>
2026-06-01 21:42:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,11 @@ st.header("2. Previsualización del dataset")
 st.dataframe(full_data.head())
 
 # 2.- Exploración inicial de Datos
-modulo = st.sidebar.selectbox("Exploración inicial de Datos.. Seleccione:", ["Relación de Clientes Activos versus Clientes que se han ido", "Relación de Años de permanencia laboral versus Clientes que se han ido", "Relación de Número de Productos versus Clientes que se han ido", "Relación de Género del Cliente versus Clientes que se han ido", "Cantidad de clientes que permanecen (0) vs. clientes que abandonaron (1)", "Graficar la distribución de edades según el estado de abandono"] )
+modulo = st.sidebar.selectbox("Exploración inicial de Datos.. Seleccione:", ["Relación de Clientes Activos versus Clientes que se han ido", 
+                                                                             "Relación de Años de permanencia laboral versus Clientes que se han ido", 
+                                                                             "Relación de Número de Productos versus Clientes que se han ido", 
+                                                                             "Relación de Género del Cliente versus Clientes que se han ido", 
+                                                                             "Cantidad de clientes que permanecen (0) vs. clientes que abandonaron (1)", "Graficar la distribución de edades según el estado de abandono", "Graficar la distribución de Balance según el estado de abandono"] )
 
 if modulo  == "Relación de Clientes Activos versus Clientes que se han ido":
     # 2.1. Relación de Miembros Activos versus Clientes que se han ido
@@ -114,4 +118,9 @@ elif modulo  == "Graficar la distribución de edades según el estado de abandon
     ax.set_xlabel("Edad")
     ax.set_ylabel("Densidad")
     st.pyplot(fig)
-
+elif modulo == "Graficar la distribución de Balance según el estado de abandono":
+    sns.kdeplot(data=full_data,x="Balance",hue="Exited",fill=True)
+    ax.set_title("Distribución de Balance según el estado de abandono")
+    ax.set_xlabel("Balance")
+    ax.set_ylabel("Densidad")
+    st.pyplot(fig)
