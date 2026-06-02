b22ae16fcf5a0deef4869e75a18f5f24326981d9
ivetterojas-afk <ivette.rojas@casagrande.edu.ec>
2026-06-01 22:33:40 -0500

    Change test size from 0.1 to 0.5 in train_test_split

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ if moduloPresentacionResultados  == "Modelo 1":
         X_train, X_test, y_train, y_test = train_test_split(
             X,
             y,
-            test_size=0.1,
+            test_size=0.5,
             random_state=42
         )
         
