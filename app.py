3e2fe5ad3b3d78c13f540c9065a491cbe6eb228a
ivetterojas-afk <ivette.rojas@casagrande.edu.ec>
2026-06-01 22:32:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ if moduloPresentacionResultados  == "Modelo 1":
         X_train, X_test, y_train, y_test = train_test_split(
             X,
             y,
-            test_size=0.2,
+            test_size=0.1,
             random_state=42
         )
         
